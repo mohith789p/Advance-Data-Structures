e9422bbe0b28d50921dd92567e9469578d54124b
Potnuru Mohith <mohith321p@gmail.com>
2025-02-20 08:45:41 +0530

    Update BTree.java

--- BTree.java
+++ BTree.java
@@ -42,6 +42,7 @@ class BTree {
         if (root.numKeys == ORDER - 1) {
             Node newRoot = new Node();
             newRoot.children[0] = this.root;
+            newRoot.isLeaf = false;
             split(newRoot, 0);
             this.root = newRoot;
         }
@@ -62,8 +63,7 @@ class BTree {
                 i--;
             }
             i++;
-            Node child = node.children[i];
-            if (child.numKeys == ORDER - 1) {
+            if (node.children[i].numKeys == ORDER - 1) {
                 split(node, i);
                 if (key > node.keys[i]) {
                     i++;
@@ -76,35 +76,44 @@ class BTree {
     private void split(Node parent, int index) {
         Node fullNode = parent.children[index];
         Node newNode = new Node();
+        newNode.isLeaf = fullNode.isLeaf;
 
-        parent.keys[index] = fullNode.keys[fullNode.numKeys / 2];
+        int mid = fullNode.numKeys / 2;
+        int midKey = fullNode.keys[mid];
+
+        // Shift parent keys and children
+        for (int j = parent.numKeys; j > index; j--) {
+            parent.keys[j] = parent.keys[j - 1];
+            parent.children[j + 1] = parent.children[j];
+        }
+
+        parent.keys[index] = midKey;
         parent.numKeys++;
 
-        newNode.isLeaf = fullNode.isLeaf;
-        for (int i = fullNode.numKeys / 2 + 1; i < fullNode.numKeys; i++) {
-            newNode.keys[i - fullNode.numKeys / 2 - 1] = fullNode.keys[i];
-            fullNode.keys[i] = 0;
+        // Move second half of keys to newNode
+        for (int j = mid + 1, k = 0; j < fullNode.numKeys; j++, k++) {
+            newNode.keys[k] = fullNode.keys[j];
+            fullNode.keys[j] = 0; // Clear old keys
         }
-        newNode.numKeys = fullNode.numKeys / 2;
+        newNode.numKeys = fullNode.numKeys - mid - 1;
+        fullNode.numKeys = mid;
 
+        // Move second half of children to newNode
         if (!fullNode.isLeaf) {
-            for (int i = fullNode.numKeys / 2 + 1; i < ORDER; i++) {
-                newNode.children[i - fullNode.numKeys / 2 - 1] = fullNode.children[i];
-                fullNode.children[i] = null;
+            for (int j = mid + 1, k = 0; j <= ORDER - 1; j++, k++) {
+                newNode.children[k] = fullNode.children[j];
+                fullNode.children[j] = null;
             }
         }
 
-        fullNode.numKeys = fullNode.numKeys / 2;
-
-        for (int i = parent.numKeys - 1; i > index; i--) {
-            parent.children[i + 1] = parent.children[i];
-            parent.keys[i] = parent.keys[i - 1];
-        }
         parent.children[index + 1] = newNode;
     }
 
     public void delete(int key) {
         delete(root, key);
+        if (root.numKeys == 0 && !root.isLeaf) {
+            root = root.children[0]; // Root becomes its only child
+        }
     }
 
     private void delete(Node node, int key) {
@@ -148,28 +157,24 @@ class BTree {
             right.children[i] = null;
         }
 
-        parent.keys[index] = 0;
+        left.numKeys += right.numKeys;
         parent.numKeys--;
 
-        for (int i = index + 1; i < parent.numKeys; i++) {
-            parent.children[i] = parent.children[i + 1];
+        for (int i = index; i < parent.numKeys; i++) {
             parent.keys[i] = parent.keys[i + 1];
+            parent.children[i + 1] = parent.children[i + 2];
         }
-
-        right.numKeys = 0;
-        Arrays.fill(right.keys, 0);
-        Arrays.fill(right.children, null);
     }
 
     public void display() {
-        // display(root, "", true);
         display(root, 0);
     }
 
     private void display(Node node, int level) {
         if (node != null) {
+            System.out.print("Level " + level + ": ");
             for (int i = 0; i < node.numKeys; i++) {
-                System.out.print("Level " + level + " Key: " + node.keys[i] + " ");
+                System.out.print(node.keys[i] + " ");
             }
             System.out.println();
             if (!node.isLeaf) {
@@ -184,44 +189,24 @@ class BTree {
 
     public static void main(String[] args) {
         BTree bTree = new BTree();
+        Set<Integer> insertedKeys = new HashSet<>();
         Random rand = new Random();
 
-        for (int i = 0; i < 100; i++) {
-            bTree.insert(rand.nextInt(1000));
+        while (insertedKeys.size() < 50) {
+            int num = rand.nextInt(100);
+            if (!insertedKeys.contains(num)) {
+                insertedKeys.add(num);
+                bTree.insert(num);
+            }
         }
 
+        System.out.println("B-Tree after insertion:");
         bTree.display();
 
-        System.out.println("Searching for key 50: " + bTree.search(50));
-
-        bTree.delete(50);
-        System.out.println("After deleting key 50:");
+        int deleteKey = insertedKeys.iterator().next(); // Pick first key for deletion
+        System.out.println("Deleting key: " + deleteKey);
+        bTree.delete(deleteKey);
+        System.out.println("B-Tree after deletion:");
         bTree.display();
     }
 }
-
-/*
- * private void display(Node node, String indent, boolean isRight) {
- * if (node != null) {
- * // Print the current node with indentation and proper label (L---- or R----)
- * System.out.print(indent);
- * if (isRight) {
- * System.out.print("R----");
- * indent += "   "; // Add space for further indentation if it's a right child
- * } else {
- * System.out.print("L----");
- * indent += "|  "; // Add '|' to show left-child connections
- * }
- * System.out.println(node.keys[0]); // Display the node's key (Assuming there's
- * at least one key)
- * 
- * // Recursively display the left and right children
- * if (node.children[0] != null) {
- * display(node.children[0], indent, false); // Left child
- * }
- * if (node.children[1] != null) {
- * display(node.children[1], indent, true); // Right child
- * }
- * }
- * }
- */
