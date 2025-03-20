8375da5381e361ad7df52d0fd12b162519169d51
Potnuru Mohith <mohith321p@gmail.com>
2025-03-20 15:02:49 +0530

    Update bfs.java

--- BFS and DFS/bfs.java
+++ BFS and DFS/bfs.java
@@ -1,46 +1,45 @@
 import java.util.*;
 
-class BFS {
-    static void bfs(List<List<Integer>> adj, int s) {
-        Queue<Integer> q = new LinkedList<>();
-        boolean[] visited = new boolean[adj.size()];
-
-        visited[s] = true;
-        q.add(s);
-
-        while (!q.isEmpty()) {
-            int curr = q.poll();
-            System.out.print(curr + " ");
-
-            for (int x : adj.get(curr)) {
-                if (!visited[x]) {
-                    visited[x] = true;
-                    q.add(x);
-                }
-            }
-        }
-    }
-
-    static void addEdge(List<List<Integer>> adj, int u, int v) {
-        adj.get(u).add(v);
-        adj.get(v).add(u);
-    }
-
+class BFS{
     public static void main(String[] args) {
-        int V = 5;
-        List<List<Integer>> adj = new ArrayList<>(V);
-        for (int i = 0; i < V; i++) {
+        int v = 4;
+        List<List<Integer>> adj = new ArrayList<>(v);
+        
+        for(int i = 0; i < v; i++){
             adj.add(new ArrayList<>());
         }
 
         addEdge(adj, 0, 1);
         addEdge(adj, 0, 2);
         addEdge(adj, 1, 3);
-        addEdge(adj, 1, 4);
-        addEdge(adj, 2, 4);
+        addEdge(adj, 2, 3);
 
-        System.out.println("BFS starting from 0:");
-        bfs(adj, 4);
+        System.out.println("BFS starts from 3:");
+        bfs(adj,1);
         System.out.println();
+
+    }
+
+    static void addEdge(List<List<Integer>> adj, int u, int v){
+        adj.get(u).add(v);
+        adj.get(v).add(u);
+    }
+
+    static void bfs(List<List<Integer>> adj, int start){
+        List<Integer> q = new ArrayList<>();
+        boolean[] visited = new boolean[adj.size()];
+        visited[start] = true;
+        q.add(start);
+        while(!q.isEmpty()){
+            int v = q.get(0);
+            System.out.print(v + " ");
+            q.remove(0);
+            for(int i : adj.get(v)){
+                if (!visited[i]) {
+                    q.add(i);
+                    visited[i] = true;                
+                }
+            }
+        }
     }
 }
