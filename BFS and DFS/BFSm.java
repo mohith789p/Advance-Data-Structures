6a2feb8bfd85bf194a36a316df62f7865da30467
Potnuru Mohith <mohith321p@gmail.com>
2025-03-25 20:25:43 +0530

    minor changes

--- BFS and DFS/BFSm.java
+++ BFS and DFS/BFSm.java
@@ -1,4 +1,5 @@
-import java.util.*;
+import java.util.Queue;
+import java.util.LinkedList;
 
 public class BFSm {
     public static void main(String[] args) {
--- BFS and DFS/bfs.java
+++ BFS and DFS/bfs.java
@@ -1,10 +1,14 @@
-import java.util.*;
-class BFS{
+import java.util.List;
+import java.util.ArrayList;
+import java.util.Queue;
+import java.util.LinkedList;
+
+class BFS {
     public static void main(String[] args) {
         int v = 4;
         List<List<Integer>> adj = new ArrayList<>(v);
-        
-        for(int i = 0; i < v; i++){
+
+        for (int i = 0; i < v; i++) {
             adj.add(new ArrayList<>());
         }
 
@@ -14,29 +18,29 @@ class BFS{
         addEdge(adj, 2, 3);
 
         System.out.println("BFS starts from 3:");
-        bfs(adj,1);
+        bfs(adj, 1);
         System.out.println();
 
     }
 
-    static void addEdge(List<List<Integer>> adj, int u, int v){
+    static void addEdge(List<List<Integer>> adj, int u, int v) {
         adj.get(u).add(v);
         adj.get(v).add(u);
     }
 
-    static void bfs(List<List<Integer>> adj, int start){
+    static void bfs(List<List<Integer>> adj, int start) {
         Queue<Integer> q = new LinkedList<>();
         boolean[] visited = new boolean[adj.size()];
         visited[start] = true;
         q.add(start);
-        while(!q.isEmpty()){
+        while (!q.isEmpty()) {
             int v = q.poll();
             System.out.print(v + " ");
 
-            for(int i : adj.get(v)){
+            for (int i : adj.get(v)) {
                 if (!visited[i]) {
                     q.add(i);
-                    visited[i] = true;                
+                    visited[i] = true;
                 }
             }
         }
